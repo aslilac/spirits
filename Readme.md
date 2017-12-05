1c4281d19ccb07e8e1e70a70144b7715558b29a3
Kayla <tylermwashburn@gmail.com>
2017-12-05 13:16:32 -0700

    Make this more neat!

--- Readme.md
+++ Readme.md
@@ -1,5 +1,5 @@
 # Spirits
-Get the spirit of strings without needing the whole string!
+Get the spirit of a string, without the whole thing!
 
 ## Usage
 ```JavaScript
@@ -12,5 +12,23 @@ if ( format.match( greeting ) ) console.log( greeting )
 else console.log( 'I have no idea what the weather is like because I am just a computer!' )
 ```
 
-### But..but that's not very wild at all..
-I know. I might make it better later.
+#### Spirit#match( string )
+Returns a boolean indicating if the string has the right spirit!
+
+#### Spirit#allMatches( strings[] )
+Returns a filtered array of all the strings that matching
+
+#### Spirit#toString()
+Returns the string used to create the spirit.
+
+### Static methods
+
+#### Spirit.match( spirit: string, string )
+Useful if you want to safely match, but you may have a string or a spirit. If the first argument
+is a string it will be used to create a spirit.
+
+#### Spirit.bestMatch( spirits[], string )
+Returns the spirit that best matches the string.
+
+#### Spirit.map( spirits[], strings[] )
+Returns a map that links strings to an array of all the spirits that match with them.
--- spirits.js
+++ spirits.js
@@ -6,14 +6,15 @@ let garden = require( 'gardens' ).auto( __filename )
 
 module.exports = class Spirit {
   constructor( string ) {
-    if ( typeof string !== 'string' ) throw new TypeError( 'Spirits are made out of strings!')
-    if ( !/^[A-Za-z0-9\*\[\]\.\_\-\:]+$/.test( string ) ) throw new Error( 'Invalid character in Spirit!' )
+    let invalid = /^[A-Za-z0-9\?\*\[\]\.\_\-\:]+$/.exec( string )
+    if ( typeof string !== 'string' ) throw garden.typeerror( 'Spirits are made out of strings!')
+    if ( invalid ) throw garden.error( `Invalid character ${invalid[0]} in Spirit!` )
 
     let stars = 0
     this.expression = new RegExp( `^${
   		string
   		  .replace( /([\.\[\]])/g, c => `\\${c}` ) // Make sure that special characters are escaped
-  		  .replace( /\*/g, ( stars++, '.*' ) ) // Turn * wildcards into RegExp wildcards
+  		  .replace( /\*/g, () => ( stars++, '.*' ) ) // Turn * wildcards into RegExp wildcards
         .replace( /\?$/, '.?' ) // If a ? is at the end of a string, the character is optional
         .replace( /\?/g, '.' ) // If it's anywhere else, the character is required.
   	}$` )
@@ -22,8 +23,18 @@ module.exports = class Spirit {
     this.spirit = string.length - stars
   }
 
+  static createSpirit( string ) {
+    return new Spirit( string )
+  }
+
   match( string ) {
+    if ( typeof string !== 'string' ) throw garden.typeerror( 'Spirits can only match against strings!' )
     return this.expression.exec( string )
+  }
+
+  allMatches( strings ) {
+    if ( !Array.isArray ) throw garden.typeerror( 'Spirit#allMatches expects an array of strings!' )
+    strings.filter( string => !!this.match( string ) )
   }
 
   toString() {
@@ -31,21 +42,33 @@ module.exports = class Spirit {
   }
 
   static match( spirit, string ) {
+    let s = spirit instanceof Spirit ? spirit : new Spirit( spirit )
     return new Spirit( spirit ).match( string )
   }
 
-  static bestMatch( string, cards ) {
-    let match = ""
+  static bestMatch( spirits, string ) {
+    if ( !Array.isArray( spirits ) ) throw garden.typeerror( 'Spirit.bestMatch expects an array of Spirits!' )
 
-    cards.forEach( card => {
-      let s = card instanceof Spirit ? card : new Spirit( card )
+    let match = new Spirit( '' )
+
+    spirits.forEach( spirit => {
+      let s = spirit instanceof Spirit ? spirit : new Spirit( spirit )
       if ( s.match( string ) && s.spirit > match.length ) match = s
     } )
 
     return match
   }
 
-  static createSpirit( string ) {
-    return new Spirit( string )
+  static map( spirits, strings ) {
+    if ( !(Array.isArray( spirits ) && Array.isArray( strings )) )
+      throw garden.typeerror( 'Spirit.map expects an array of spirits and an array of strings!' )
+
+    return new Map(
+      strings.map( string => {
+        return [ string, spirits.filter( spirit =>
+          !!(( spirit instanceof Spirit ? spirit : new Spirit( spirit ) ).match( string ))
+        ) ]
+      })
+    )
   }
 }
