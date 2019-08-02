6adbc161152f5483b847f0656c1d19324d3dd90d
McKayla Washburn <partheseas@gmail.com>
2019-08-01 21:50:02 -0600

    Remove some comments

--- lib/spirits.ts
+++ lib/spirits.ts
@@ -126,48 +126,13 @@ export default class Spirit {
   }
 }
 
-// export default class Spirit {
-//   constructor( string ) {
-//     let invalid = /^[^A-Za-z0-9\?\*\[\]\.\_\-\:]+$/.exec( string )
-//     if ( typeof string !== 'string' ) throw garden.typeerror( 'Spirits are made out of strings!')
-//     if ( invalid ) throw garden.error( `Invalid character ${invalid[0]} in Spirit!` )
-
-//     let stars = 0
-//     this.expression = new RegExp( `^${
-//   		string
-//   		  .replace( /([\.\[\]])/g, c => `\\${c}` ) // Make sure that special characters are escaped
-//   		  .replace( /\*/g, () => ( stars++, '.*' ) ) // Turn * wildcards into RegExp wildcards
-//         .replace( /\?/g, '.' ) // If it's anywhere else, the character is required.
-//         .replace( /\.$/, '.?' ) // If a ? is at the end of a string, the character is optional
-//   	}$` )
-
-//   	this._string = string
-//     this.spirit = string.length - stars
-//   }
-
-//   static bestMatch( spirits, string ) {
-//     if ( !Array.isArray( spirits ) ) throw garden.typeerror( 'Spirit.bestMatch expects an array of Spirits!' )
-
-//     let match = null
-
-//     spirits.forEach( item => {
-//       let spirit = item instanceof Spirit ? item : new Spirit( item )
-//       if ( spirit.match( string ) ) {
-//         if ( !match ) match = spirit
-//         else if ( spirit.spirit > match.spirit ) match = spirit
-//       }
-//     } )
-
-//     return match
-//   }
-
 //   static map( spirits, strings ) {
 //     if ( !(Array.isArray( spirits ) && Array.isArray( strings )) )
 //       throw garden.typeerror( 'Spirit.map expects an array of spirits and an array of strings!' )
 
 //     return new Map(
 //       strings.map( string => {
-//         return [ string, spirits.filter( spirit =>
+//         return [ string, ...spirits.filter( spirit =>
 //           !!(( spirit instanceof Spirit ? spirit : new Spirit( spirit ) ).match( string ))
 //         ) ]
 //       })
